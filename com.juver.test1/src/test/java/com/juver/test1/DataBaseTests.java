a6a4a6db5d8b8b872789063e6ed1f83e70ea6ccf
JUVER MALPARTIDA <JUVERCASTRO@hotmail.com>
2018-05-12 01:21:23 -0500

    Using Autowired.

--- com.juver.test1/src/test/java/com/juver/test1/DataBaseTests.java
+++ com.juver.test1/src/test/java/com/juver/test1/DataBaseTests.java
@@ -1,31 +1,40 @@
 package com.juver.test1;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import database.EventRepository;
 import database.Events_event;
-import static org.hamcrest.Matchers.*;
-import static io.restassured.RestAssured.*;
 
 @SpringBootApplication
 @EnableJpaRepositories("database")
 @EntityScan("database")
+@RunWith(SpringJUnit4ClassRunner.class)
+@PropertySource("classpath:application-test.properties")
 public class DataBaseTests {
+	
 	private static final Logger log = LoggerFactory.getLogger(DataBaseTests.class);
+	
+	@Autowired
+	private EventRepository eventRepository;
 
 	@Test
 	public void QueryPostgressDb() {
 		log.info("Arrange");
 		int eventId = 19;
-		ConfigurableApplicationContext context = SpringApplication.run(DataBaseTests.class);
-		EventRepository repository = context.getBean(EventRepository.class);
-		Events_event event = repository.findById(eventId).orElse(null);
+		Events_event event = eventRepository.findById(eventId).orElse(null);
+		
 		log.info("Act-Assert");
 		given()
 			.pathParam("eventId", eventId)
